5273b112c6da8db16a89028e8fc4a9ed73c0730b
roboshop <j.devops05324u@gmail.com>
2024-10-17 22:18:07 +0530

    upgrading code

--- common.sh
+++ common.sh
@@ -1,11 +1,11 @@
 log=/tmp/roboshop.log
-script_location=${pwd}
+script_location=$(pwd)
 
 status_check() {
-  if [$? -eq 0]; then
+  if [ $? -eq 0 ]; then
     echo -e "\e[31m success\e[0m"
   else
-    echo "failure, refer log at -$(log)"
+    echo "failure, refer log at -${log}"
   fi
 }
 header() {
